8e60e4be0d571c54835b3ddb501573e1f2f493aa
VitorFaria <vitorfaria963@hotmail.com>
2024-12-01 06:30:40 -0300

    Add fillable fields and relation between tables

--- app/Models/Patient.php
+++ app/Models/Patient.php
@@ -5,8 +5,35 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Patient extends Model
 {
     use HasFactory, HasUuids;
+
+    protected $fillable = [
+        'patient_type',
+        'blood_type'
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function address(): BelongsTo
+    {
+        return $this->belongsTo(Address::class);
+    }
+
+    public function hospitals(): BelongsToMany
+    {
+        return $this->belongsToMany(Hospital::class);
+    }
+
+    public function organs(): BelongsToMany
+    {
+        return $this->belongsToMany(Organ::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -44,4 +45,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function patient(): HasOne
+    {
+        return $this->hasOne(Patient::class);
+    }
 }
