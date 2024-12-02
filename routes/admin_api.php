82c450b05195aab0eb04ce65dfe86bf59d9361f6
VitorFaria <vitorfaria963@hotmail.com>
2024-12-02 03:05:42 -0300

    Add custom middleware to admin routes

--- routes/admin_api.php
+++ routes/admin_api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\Admin\AdminAuthController;
 use App\Http\Controllers\Api\Admin\AdminOrganController;
 use App\Http\Controllers\Api\HospitalController;
+use App\Http\Middleware\CheckRoleBeforeAction;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('auth')->group(function() {
@@ -10,17 +11,19 @@ Route::prefix('auth')->group(function() {
 });
 
 Route::middleware('auth:sanctum')->group(function() {
-  Route::post('logout', [AdminAuthController::class, 'logout']);
-  Route::get('me', [AdminAuthController::class, 'me']);
+  Route::middleware([CheckRoleBeforeAction::class])->group(function() {
+    Route::post('logout', [AdminAuthController::class, 'logout']);
+    Route::get('me', [AdminAuthController::class, 'me']);
 
-  Route::prefix('organs')->group(function() {
-    Route::get('', [AdminOrganController::class, 'index']);
-    Route::get('/{id}', [AdminOrganController::class, 'show']);
-    Route::post('', [AdminOrganController::class, 'store']);
-    Route::patch('/{id}', [AdminOrganController::class, 'update']);
-  });
+    Route::prefix('organs')->group(function() {
+      Route::get('', [AdminOrganController::class, 'index']);
+      Route::get('/{id}', [AdminOrganController::class, 'show']);
+      Route::post('', [AdminOrganController::class, 'store']);
+      Route::patch('/{id}', [AdminOrganController::class, 'update']);
+    });
 
-  Route::prefix('hospitals')->group(function() {
-    Route::post('store', [HospitalController::class, 'store']);
+    Route::prefix('hospitals')->group(function() {
+      Route::post('store', [HospitalController::class, 'store']);
+    });
   });
 });
