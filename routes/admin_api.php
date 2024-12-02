0c3fb03ee833c788e7be8fc8f3180f43a8c12ba8
VitorFaria <vitorfaria963@hotmail.com>
2024-12-02 05:20:11 -0300

    Create address and hospital endpoints inside admin api routes

--- routes/admin_api.php
+++ routes/admin_api.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Http\Controllers\AddressController;
 use App\Http\Controllers\Api\Admin\AdminAuthController;
 use App\Http\Controllers\Api\Admin\AdminOrganController;
-use App\Http\Controllers\Api\HospitalController;
+use App\Http\Controllers\Api\Admin\HospitalController;
 use App\Http\Middleware\CheckRoleBeforeAction;
 use Illuminate\Support\Facades\Route;
 
@@ -15,6 +16,13 @@ Route::middleware('auth:sanctum')->group(function() {
     Route::post('logout', [AdminAuthController::class, 'logout']);
     Route::get('me', [AdminAuthController::class, 'me']);
 
+    Route::prefix('addresses')->group(function() {
+      Route::get('', [AddressController::class, 'index']);
+      Route::get('/{id}', [AddressController::class, 'show']);
+      Route::post('', [AddressController::class, 'store']);
+      Route::patch('/{id}', [AddressController::class, 'update']);
+    });
+
     Route::prefix('organs')->group(function() {
       Route::get('', [AdminOrganController::class, 'index']);
       Route::get('/{id}', [AdminOrganController::class, 'show']);
@@ -23,7 +31,10 @@ Route::middleware('auth:sanctum')->group(function() {
     });
 
     Route::prefix('hospitals')->group(function() {
-      Route::post('store', [HospitalController::class, 'store']);
+      Route::get('', [HospitalController::class, 'index']);
+      Route::get('/{id}', [HospitalController::class, 'show']);
+      Route::post('', [HospitalController::class, 'store']);
+      Route::patch('/{id}', [HospitalController::class, 'update']);
     });
   });
 });
