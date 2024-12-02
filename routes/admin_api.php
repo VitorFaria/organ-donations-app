3f0086e81fd31ea7446f3a79ada92a0fda5f4a2e
VitorFaria <vitorfaria963@hotmail.com>
2024-12-02 00:35:39 -0300

    Add organ endpoints into admin routes

--- routes/admin_api.php
+++ routes/admin_api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\Admin\AdminAuthController;
+use App\Http\Controllers\Api\Admin\AdminOrganController;
 use App\Http\Controllers\Api\HospitalController;
 use Illuminate\Support\Facades\Route;
 
@@ -12,7 +13,14 @@ Route::middleware('auth:sanctum')->group(function() {
   Route::post('logout', [AdminAuthController::class, 'logout']);
   Route::get('me', [AdminAuthController::class, 'me']);
 
+  Route::prefix('organs')->group(function() {
+    Route::get('', [AdminOrganController::class, 'index']);
+    Route::get('/{id}', [AdminOrganController::class, 'show']);
+    Route::post('', [AdminOrganController::class, 'store']);
+    Route::patch('/{id}', [AdminOrganController::class, 'update']);
+  });
+
   Route::prefix('hospitals')->group(function() {
-    Route::post('store', [HospitalController::class, 'store'])->name('hospital.store');
+    Route::post('store', [HospitalController::class, 'store']);
   });
 });
