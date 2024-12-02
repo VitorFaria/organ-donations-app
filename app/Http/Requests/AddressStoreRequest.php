f6b74c67bc6d2599d942cea4a35997b351e7befb
VitorFaria <vitorfaria963@hotmail.com>
2024-12-02 05:21:44 -0300

    Create address store and update files to handle validation before saving

---
+++ app/Http/Requests/AddressStoreRequest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AddressStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'zip_code' => 'required|string|size:8',
+            'street' => 'required|string|min:5|max:100',
+            'neighbourhood' => 'required|string|min:5|max:100',
+            'state' => 'required|string|size:2',
+            'city' => 'required|string|min:5|max:100',
+            'house_number' => 'required|string|min:1|max:100',
+            'complement' => 'nullable|string|min:3|max:100',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'required' => 'Este campo é obrigatório',
+            'string' => 'Este campo deve ser um texto',
+            'min' => 'Este campo deve conter no mínimo :min caracteres',
+            'max' => 'Este campo deve conter no máximo :max caracteres',
+            'size' => 'Este campo deve conter exatamente :size caracteres',
+        ];
+    }
+
+    public function prepareForValidation()
+    {
+        $zipCodeWithoutHyphen = str_replace('-', '', $this->zip_code);
+
+        $this->merge([
+            'zip_code' => $zipCodeWithoutHyphen
+        ]);
+    }
+}
