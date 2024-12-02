bdb4584b40302ae59766f43150ee6346da22b10b
VitorFaria <vitorfaria963@hotmail.com>
2024-12-02 10:16:37 -0300

    Create an address factory and include in seeds

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             AdminSeeder::class,
+            AddressSeeder::class,
             OrganSeeder::class,
         ]);
     }
