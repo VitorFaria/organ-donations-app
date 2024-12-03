c57bbe0d6f6f35eeebc8f5a9d394d2b0cb654e8d
VitorFaria <vitorfaria963@hotmail.com>
2024-12-03 16:45:34 -0300

    Create repository to get the patient details

---
+++ app/Repositories/PatientRepository.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Patient;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class PatientRepository extends BaseRepository
+{
+  public function patientDetails(): array
+  {
+    $user = Auth::user();
+    $patient = $user->patient()->first();
+
+    $hasOrgansSelected = false;
+    if (!empty($patient)) {
+      $organs = $patient->organs()->get();
+      $patient['organs'] = $organs;
+      $hasOrgansSelected = $organs->count() > 0;
+    }
+    
+    return [
+      'user' => $user,
+      'patient' => $patient,
+      'hasOrgansSelected' => $hasOrgansSelected
+    ];
+  }
+
+  public function findPatient(string $userId): Patient
+  {
+    $user = User::find($userId);
+    $patient = $user->patient()->first();
+
+    return $patient;
+  }
+}
