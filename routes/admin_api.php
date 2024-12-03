4b1b097c0b7477e96678e39cca69e196a6fbf749
VitorFaria <vitorfaria963@hotmail.com>
2024-12-03 17:29:16 -0300

    Remove admin login routes

--- routes/admin_api.php
+++ routes/admin_api.php
@@ -4,19 +4,11 @@ use App\Http\Controllers\Api\AddressController;
 use App\Http\Controllers\Api\Admin\AdminAuthController;
 use App\Http\Controllers\Api\Admin\AdminHospitalController;
 use App\Http\Controllers\Api\Admin\AdminOrganController;
-use App\Http\Controllers\Api\Admin\AdminUserController;
 use App\Http\Middleware\CheckRoleBeforeAction;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('auth')->group(function() {
-  Route::post('login', [AdminAuthController::class, 'login']);
-});
-
 Route::middleware('auth:sanctum')->group(function() {
   Route::middleware([CheckRoleBeforeAction::class])->group(function() {
-    Route::post('logout', [AdminAuthController::class, 'logout']);
-    Route::get('me', [AdminAuthController::class, 'me']);
-
     Route::prefix('addresses')->group(function() {
       Route::get('', [AddressController::class, 'index']);
       Route::get('/{id}', [AddressController::class, 'show']);
